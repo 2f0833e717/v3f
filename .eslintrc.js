ade764b72110c4ee53aa0f11d8142c61f9590016
2f0833e717 <skinoshita202001082135@gmail.com>
2020-02-17 00:49:58 +0900

    eslint setting update and fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
 
   parserOptions: {
-    // parser: "babel-eslint",
+    parser: "babel-eslint",
     sourceType: "module"
   },
 
@@ -19,7 +19,7 @@ module.exports = {
     // 'vue',
     "plugin:import/errors",
     "plugin:import/warnings",
-    "plugin:prettier/recommended",
+    // "plugin:prettier/recommended",
   ],
 
   // required to lint *.vue files
@@ -37,59 +37,59 @@ module.exports = {
 
   // add your custom rules here
   rules: {
-    // // allow async-await
-    // "generator-star-spacing": "off",
-    // // allow paren-less arrow functions
-    // "arrow-parens": "off",
-    // "one-var": "off",
+    // allow async-await
+    "generator-star-spacing": "off",
+    // allow paren-less arrow functions
+    "arrow-parens": "off",
+    "one-var": "off",
 
-    // "import/first": "off",
-    // "import/named": "error",
-    // "import/namespace": "error",
-    // "import/default": "error",
-    // "import/export": "error",
-    // "import/extensions": "off",
-    // "import/no-unresolved": "off",
-    // "import/no-extraneous-dependencies": "off",
-    // "prefer-promise-reject-errors": "off",
+    "import/first": "off",
+    "import/named": "error",
+    "import/namespace": "error",
+    "import/default": "error",
+    "import/export": "error",
+    "import/extensions": "off",
+    "import/no-unresolved": "off",
+    "import/no-extraneous-dependencies": "off",
+    "prefer-promise-reject-errors": "off",
 
-    // // allow debugger during development only
-    // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    // allow debugger during development only
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
 
-  //   // custom rules
-  //   // https://eslint.org/docs/rules/
-  //   multiline: "never",
-  //   // 不要なカッコは消す
-  //   "no-extra-parens": 1,
-  //   // 無駄なスペースは削除
-  //   "no-multi-spaces": 2,
-  //   // 関数とカッコはあけない(function hoge() {/** */})
-  //   "func-call-spacing": [2, "never"],
-  //   // セミコロンは禁止
-  //   semi: [2, "never"],
-  //   // 文字列
-  //   quotes: [2, "double"],
-  //   // varは禁止
-  //   "no-var": 2,
-  //   // jsのインデントは２
-  //   indent: ["error", 2],
-  //   // かっこの中はスペースなし！違和感
-  //   "space-in-parens": [2, "never"],
-  //   // コンソールは許可
-  //   "no-console": 0,
-  //   // カンマの前後にスペース入れる？
-  //   "comma-spacing": 2,
-  //   // キー
-  //   "key-spacing": 2,
-  //   // キーワードの前後には適切なスペースを
-  //   "keyword-spacing": 2,
-  //   // <component> indent 2
-  //   "vue/html-indent": ["error", 2],
-  //   //
-  //   "space-before-function-paren": [0, "never"],
-    // prettier
-    "rules": {
-      "prettier/prettier": "error"
-    }
+    // custom rules
+    // https://eslint.org/docs/rules/
+    multiline: "never",
+    // 不要なカッコは消す
+    "no-extra-parens": 1,
+    // 無駄なスペースは削除
+    "no-multi-spaces": 2,
+    // 関数とカッコはあけない(function hoge() {/** */})
+    "func-call-spacing": [2, "never"],
+    // セミコロンは禁止
+    semi: [2, "never"],
+    // 文字列
+    quotes: [2, "double"],
+    // varは禁止
+    "no-var": 2,
+    // jsのインデントは２
+    indent: ["error", 2],
+    // かっこの中はスペースなし！違和感
+    "space-in-parens": [2, "never"],
+    // コンソールは許可
+    "no-console": 0,
+    // カンマの前後にスペース入れる？
+    "comma-spacing": 2,
+    // キー
+    "key-spacing": 2,
+    // キーワードの前後には適切なスペースを
+    "keyword-spacing": 2,
+    // <component> indent 2
+    "vue/html-indent": ["error", 2],
+    //
+    "space-before-function-paren": [0, "never"],
+    // // prettier
+    // "rules": {
+    //   "prettier/prettier": "error"
+    // }
   }
 };
